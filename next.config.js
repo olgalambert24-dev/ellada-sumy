b7538bb49b114b796edc721ce6fb11f8cc6588d7
Prune <olgalambert24@gmail.com>
2026-05-17 12:05:49 +0200

    Remove redirect from vercel.app to com.ua

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'ellada-sumy.vercel.app',
-          },
-        ],
-        destination: 'https://ellada-sumy.com.ua/:path*',
-        permanent: true,
-      },
-    ];
+    return [];
   },
 };
 
